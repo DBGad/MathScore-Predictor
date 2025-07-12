db418a43accd4ce049e49df321895cdd16499768
DBGad <gadelhaq.work@gmail.com>
2025-07-12 19:20:19 +0300

    edit and fix some issues

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,13 @@
 import sys 
 import os 
 import pandas as pd 
-##project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-##sys.path.append(project_root)
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(project_root)
 
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -52,4 +54,7 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
     
