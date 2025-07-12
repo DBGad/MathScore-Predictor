b19a13be6794f9905b97b700f574d66a4e709e5b
DBGad <gadelhaq.work@gmail.com>
2025-07-12 23:07:34 +0300

    Edit and Fix some issues

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import pickle
 
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -17,4 +18,22 @@ def save_object(file_path,obj):
             pickle.dump(obj,file_obj)
 
     except Exception as e :
+        raise CustomException(e,sys)
+    
+
+def evaluate_model(x_train,y_train, x_test , y_test, models:dict):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(x_train,y_train)
+
+            y_test_pred = model.predict(x_test)
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+            return report
+    except Exception as e:
         raise CustomException(e,sys)
